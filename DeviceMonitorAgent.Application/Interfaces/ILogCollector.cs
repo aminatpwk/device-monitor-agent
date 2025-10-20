c0da8f74fade0b58158af2cb2f31e65c3bf87153
unknown <aminasokoli0602@outlook.com>
2025-10-20 22:33:01 +0200

    implemented logging logic based on OS

---
+++ DeviceMonitorAgent.Application/Interfaces/ILogCollector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceMonitorAgent.Application.Interfaces
+{
+    public interface ILogCollector
+    {
+        /// <summary>
+        /// collects and zips the logs, returning the path to the zipped file
+        /// </summary>
+        /// <returns></returns>
+        string CollectAndZipLogs();
+    }
+}
---
+++ DeviceMonitorAgent.Infrastructure/SystemTools/LogCollector.cs
@@ -0,0 +1,31 @@
+﻿using DeviceMonitorAgent.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceMonitorAgent.Infrastructure.SystemTools
+{
+    public class LogCollector : ILogCollector
+    {
+        public string CollectAndZipLogs()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Console.WriteLine("Windows Log Collection: Simulating Event Source retrieval and zipping.");
+                return "C:\\Temp\\agent_logs_windows.zip";
+            }else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Console.WriteLine("Linux Log Collection: Simulating SystemD log retrieval and zipping.");
+                return "/var/log/agent_logs_linux.zip";
+            }
+            else
+            {
+                Console.WriteLine($"Log collection not implemented for OS: {RuntimeInformation.OSDescription}");
+                return string.Empty;
+            }
+        }
+    }
+}
